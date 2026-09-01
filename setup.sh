16b3f180f5eb61240a34eebd4ffc025672f3f06b
Szymon Bednorz <dsonyyy@gmail.com>
2026-09-01 15:37:54 +0200

    chore: gh

--- setup.sh
+++ setup.sh
@@ -102,6 +102,17 @@ if ! command -v handy &> /dev/null; then
     sudo apt install -y /tmp/handy.deb && rm /tmp/handy.deb
 fi
 
+# github
+(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+
 ############## SETUP
 
 export DOTFILES=$HOME/repos/dotfiles
